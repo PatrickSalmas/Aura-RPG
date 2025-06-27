56b9dc57d28c5bbd49d2fac026fa5fd510203605
Patrick Salmas <patricksalmas@gmail.com>
2025-06-26 21:41:14 -0700

    Derived Attributes

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -11,10 +11,10 @@
 
 UAuraAttributeSet::UAuraAttributeSet()
 {
-	InitHealth(50.f);
-	InitMaxHealth(100.f);
-	InitMana(25.f);
-	InitMaxMana(50.f);
+	InitHealth(10.f);
+	InitMaxHealth(10.f);
+	// InitMana(25.f);
+	// InitMaxMana(50.f);
 }
 
 void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
