a6ad01dfef8cc0d0c7aa4817664155916305ed68
Patrick Salmas <patricksalmas@gmail.com>
2025-06-28 09:58:50 -0700

    Aura Asset Manager

---
+++ Source/Aura/Private/AuraAssetManager.cpp
@@ -0,0 +1,20 @@
+// Copyright Salmas Coporation
+
+
+#include "AuraAssetManager.h"
+#include "AuraGameplayTags.h"
+
+UAuraAssetManager& UAuraAssetManager::Get()
+{
+	check(GEngine);
+	
+	UAuraAssetManager* AuraAssetManager = Cast<UAuraAssetManager>(GEngine->AssetManager);
+	return *AuraAssetManager;
+}
+
+void UAuraAssetManager::StartInitialLoading()
+{
+	Super::StartInitialLoading();
+
+	FAuraGameplayTags::InitializeNativeGameplayTags();
+}
