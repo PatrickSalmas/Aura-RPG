d1d336d75f450dafa37428ac71764713d9feced5
Patrick Salmas <patricksalmas@gmail.com>
2025-06-30 11:58:43 -0700

    Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,12 @@
+// Copyright Salmas Corporation
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+}
+
+void UAttributeMenuWidgetController::BindCallbackToDependencies()
+{
+}
