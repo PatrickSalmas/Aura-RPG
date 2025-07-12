b8a2c6d551d101f2456ee7ba0a05545822c6955b
Patrick Salmas <patricksalmas@gmail.com>
2025-07-11 21:29:37 -0700

    Character Class Info

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyright Salmas Corporation
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
