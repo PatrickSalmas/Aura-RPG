019017fcbb5d7a6414e311334ae4a3d081f5a99c
Patrick Salmas <patricksalmas@gmail.com>
2025-09-07 19:52:47 -0700

    Enemy Hit React (136 & 137)
    136. Enemy Hit React
    137. Activating the Enemy Hit React Ability

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -12,6 +12,7 @@
 class UAuraAbilitySystemComponent;
 class UAbilitySystemComponent;
 class UAttributeSet;
+class UAnimMontage;
 
 UCLASS(Abstract)
 class AURA_API AAuraCharacterBase : public ACharacter, public IAbilitySystemInterface, public ICombatInterface
@@ -24,6 +25,7 @@ public:
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 	UAttributeSet* GetAttributeSet() const { return AttributeSet; }
 
+	virtual UAnimMontage* GetHitReactMontage_Implementation() override;
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -61,4 +63,7 @@ protected:
 private:
 	UPROPERTY(EditAnywhere, Category="Abilities")
 	TArray<TSubclassOf<UGameplayAbility>> StartupAbilities;
+
+	UPROPERTY(EditAnywhere, Category="Combat")
+	TObjectPtr<UAnimMontage> HitReactMontage;
 };
