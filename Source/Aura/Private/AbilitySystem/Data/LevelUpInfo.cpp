99558fa4c480ad851559e459adc6800fd1a525dc
Patrick Salmas <patricksalmas@gmail.com>
2025-10-30 19:03:54 -0700

    Level Up Info Data Asset

---
+++ Source/Aura/Private/AbilitySystem/Data/LevelUpInfo.cpp
@@ -0,0 +1,25 @@
+// Copyright Salmas Corporation
+
+
+#include "AbilitySystem/Data/LevelUpInfo.h"
+
+int32 ULevelUpInfo::FindLevelForXP(int32 XP)
+{
+	int32 Level = 1;
+	int32 highestXPRange = XP;
+	for (int32 i = 0; i < LevelUpInformation.Num(); i++)
+	{
+		if (LevelUpInformation.Num() <= Level) return Level;
+		if (highestXPRange >= LevelUpInformation[i].LevelUpRequirement)
+		{
+			Level = i + 1;
+			highestXPRange = LevelUpInformation[i].LevelUpRequirement;
+		}
+		else
+		{
+			return Level;
+		}
+	}
+	
+	return Level;
+}
