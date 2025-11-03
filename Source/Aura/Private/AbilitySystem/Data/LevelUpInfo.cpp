400c6458b607f55e22ecc165c4f5760dbbee8a1b
Patrick Salmas <patricksalmas@gmail.com>
2025-11-02 22:29:49 -0800

    Showing Level in the HUD

--- Source/Aura/Private/AbilitySystem/Data/LevelUpInfo.cpp
+++ Source/Aura/Private/AbilitySystem/Data/LevelUpInfo.cpp
@@ -7,10 +7,10 @@ int32 ULevelUpInfo::FindLevelForXP(int32 XP) const
 {
 	int32 Level = 1;
 	int32 highestXPRange = XP;
-	for (int32 i = 0; i < LevelUpInformation.Num(); i++)
+	for (int32 i = 1; i < LevelUpInformation.Num(); i++)
 	{
 		if (LevelUpInformation.Num() <= Level) return Level;
-		if (highestXPRange >= LevelUpInformation[i].LevelUpRequirement)
+		if (XP >= LevelUpInformation[i].LevelUpRequirement)
 		{
 			Level = i + 1;
 			highestXPRange = LevelUpInformation[i].LevelUpRequirement;
