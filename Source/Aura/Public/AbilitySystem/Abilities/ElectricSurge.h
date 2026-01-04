da89ea69cc32cde4e50fd29d5146f3cddc6ea37f
Patrick Salmas <patricksalmas@gmail.com>
2026-01-03 17:44:22 -0800

    Setting up ElectricSurge file structure in UE5 and enabling the ability through the UI

---
+++ Source/Aura/Public/AbilitySystem/Abilities/ElectricSurge.h
@@ -0,0 +1,17 @@
+// Copyright Salmas Corporation
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraBeamSpell.h"
+#include "ElectricSurge.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UElectricSurge : public UAuraBeamSpell
+{
+	GENERATED_BODY()
+	
+};
--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -91,6 +91,7 @@ public:
 	FGameplayTag Abilities_Fire_FireBolt;
 	FGameplayTag Abilities_Fire_FireBlast;
 	FGameplayTag Abilities_Lightning_Electrocute;
+	FGameplayTag Abilities_Lightning_ElectricSurge;
 	FGameplayTag Abilities_Arcane_ArcaneShards;
 	FGameplayTag Abilities_Arcane_ArcaneExplosion;
 
