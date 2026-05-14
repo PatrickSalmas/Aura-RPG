505c06f96eeb39f734477bbbc4d23997961d02f9
Patrick Salmas <patricksalmas@gmail.com>
2026-05-14 15:38:25 -0700

    LavaTurtle: Created test Niagara effect for ShieldSurge & Granted LavaTurtle (Warrior Class) ability to use ShieldSurge

--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -94,6 +94,7 @@ public:
 	FGameplayTag Abilities_Fire_FireBlast;
 	FGameplayTag Abilities_Fire_FlameFlurry;
 	FGameplayTag Abilities_Fire_FlameRupture;
+	FGameplayTag Abilities_Fire_ShieldSurge;
 	FGameplayTag Abilities_Lightning_Electrocute;
 	FGameplayTag Abilities_Lightning_ElectricSurge;
 	FGameplayTag Abilities_Lightning_ElectricDomination;
